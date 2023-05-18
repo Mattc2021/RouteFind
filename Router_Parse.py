ae8fda91a0a0271fd8ba521f83bb8bb5370b551d
Liam Griffin <133255883+liamgriffin11@users.noreply.github.com>
2023-05-17 23:03:21 -0400

    Router_Parse.py
    - Fixed issue of JSON only exporting last router config.
    - Filtered out further unnecessary info in router config

--- Router_Parse.py
+++ Router_Parse.py
@@ -1,19 +1,25 @@
 import json
-
+#Pulls router configs
 file_paths = ['Router0.txt', 'Router1.txt', 'Router2.txt', 'Router3.txt', 'Router4.txt']
-#Pulls interface and protocol data. Does not look for VPN
+#Filters out more useless information
+ignore_strings = ["no ip address", "duplex auto", "speed auto","shutdown", "log-adjacency-changes"]
+#Array to hold data for JSON outside the for loop
+all_data = []
+
 for file_path in file_paths:
     with open(file_path) as f:
         lines = []
         keep = False
         for line in f:
+            if any(ignore_string in line for ignore_string in ignore_strings):     #filters out strings
+                continue
             if line.startswith('interface') or line.startswith('router'):
                 keep = True
             elif line.startswith('!'):
                 keep = False
             if keep:
                 lines.append(line.strip())
-#Stores sorted data
+                
     data = {}
     current_key = None
     for line in lines:
@@ -23,11 +29,13 @@ for file_path in file_paths:
         else:
             data[current_key].append(line)
 
-    json_data = json.dumps(data, indent=4)
-    print(json_data) #this prints all relevant data
-    with open('configs.json', 'w') as f: 
-        f.write(json_data) #only writes data from router0
+    all_data.append(data)
 
+json_data = json.dumps(all_data, indent=4)     #fixed previous issue with data being overwritten each itteration of the for loop
+#print(json_data)
+   
+with open('configs.json', 'w') as f:
+   f.write(json_data)
 
 
  
